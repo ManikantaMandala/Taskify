a86762a0f19d5086494214a4eae0ed33d89a395c
ManikantaMandala <mandala.manikanta.20cse@bmu.edu.in>
2024-02-11 06:24:17 +0530

    changed to production env

--- backend/frontendUrl.js
+++ backend/frontendUrl.js
@@ -1,10 +1,11 @@
 const urlDev = [
     'https://localhost:3451'
-]
+];
+
 const urlProducation = [ 
     'https://todo-application-sooty-omega.vercel.app',
     'https://todo-application-git-master-manikantamandalas-projects.vercel.app',
     'https://todo-application-manikantamandalas-projects.vercel.app'
-]
+];
 
-module.exports = urlDev
+module.exports = urlProducation;
--- frontend/src/scripts/envVariable.js
+++ frontend/src/scripts/envVariable.js
@@ -1,9 +1,9 @@
 const dev = {
     backendLink: 'http://localhost:3451'
-}
+};
 
 const production ={
     backendLink: 'https://todo-application-backend.vercel.app'
-}
+};
 
-export default dev
+export default production;
